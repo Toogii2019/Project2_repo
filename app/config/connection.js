360945cc87ac0f4047f6f54dbbfd21dbf59a0bca
Toogii <dashdava@uw.edu>
2020-09-25 22:44:28 -0700

    Centered sign in

--- app/config/connection.js
+++ app/config/connection.js
@@ -9,7 +9,7 @@
 var Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-var sequelize = new Sequelize(process.env.DBNAME || "hounted_house_db", process.env.DBUNAME || "root", process.env.DBPWD || "", {
+var sequelize = new Sequelize(process.env.DBNAME || "hounted_house_db", process.env.DBUNAME || "root", process.env.DBPWD || "rootadmin", {
   host: process.env.DBHOST || "localhost",
   port: process.env.DBPORT || 3306,
   dialect: process.env.DIALECT || "mysql",
